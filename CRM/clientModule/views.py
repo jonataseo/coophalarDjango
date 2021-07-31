879611a7357a6e266a2259a43f0f6e984d388253
Jonatas Oliveira <jonatas.eoliveira@gmail.com>
2021-07-31 00:22:56 -0400

    Adding urls for some client views, adding initial template for all users

--- CRM/clientModule/views.py
+++ CRM/clientModule/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .models import Client
 # Create your views here.
+
+
+def index(request):
+    clients_list = Client.objects.order_by('full_name')[:5]
+    context = {'clients_list': clients_list}
+    return render(request, 'clientModule/index.html', context)
+
+
+def detail(request, client_id):
+    return HttpResponse("Client %s" % client_id)
+
+
