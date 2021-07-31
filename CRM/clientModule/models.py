374aca8a1e1b67d7474c0d3fd40f74b605852458
Jonatas Oliveira <jonatas.eoliveira@gmail.com>
2021-07-31 00:02:06 -0400

    Adding model for client and address, config django admin, migrations

--- CRM/clientModule/models.py
+++ CRM/clientModule/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "%s" % self.full_name
+
+
+class Address(models.Model):
+    street_name = models.CharField(max_length=200)
+    house_number = models.CharField(max_length=5)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s - %s" % (self.street_name, self.client)
+
